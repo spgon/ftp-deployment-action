364068343e1bb4f063cb885eef0fb764040b4c79
udayvatturi <udayvatturi@gmail.com>
2022-01-07 16:01:14 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -90,9 +90,9 @@ until [ ${COUNTER} -gt ${INPUT_MAX_RETRIES} ]; do
   echo ""
   echo "Try #: ${COUNTER}"
   echo "# ---------------------------------------------"
-  sftp \
+  lftp \
     --debug \
-    -u "${INPUT_USER}","${INPUT_PASSWORD}" \
+    -u '${INPUT_USER}','${INPUT_PASSWORD}' \
     "${INPUT_SERVER}" \
     -e "${FTP_SETTINGS} ${MIRROR_COMMAND} ${INPUT_LOCAL_DIR} ${INPUT_REMOTE_DIR}; quit;" &&
     SUCCESS="true"
